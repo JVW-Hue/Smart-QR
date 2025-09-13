5ec6d338d3ae3510cab931aecdabc306c5d27123
UptimeMonitor Dev <janovanwyk553@gmail.com>
2025-09-13 02:11:59 +0200

    Fixed Pillow compatibility and deployment issues

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install Django==4.2.7
+pip install qrcode==7.4.2
+pip install --no-build-isolation Pillow==10.4.0
+pip install stripe==7.8.0
+pip install python-decouple==3.8
+pip install gunicorn==21.2.0
 python manage.py collectstatic --noinput
 python manage.py migrate
